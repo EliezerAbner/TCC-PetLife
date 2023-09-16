d4b967b02583643ed1ca0b663d1281ed9f2f46de
EliezerAbner <eliezerabnerpo@gmail.com>
2023-09-16 19:16:10 -0300

    Alteração na Classe Cliente, remoção da Classe ConectaDb

--- Mobile/PetLifeApp/PetLifeApp/Models/Cliente.cs
+++ Mobile/PetLifeApp/PetLifeApp/Models/Cliente.cs
@@ -7,6 +7,8 @@ namespace PetLifeApp.Models
 {
     class Cliente
     {
+        private int id;
+
         private static string conn = @"server=sql.freedb.tech;port=3306;database=freedb_matadoresDePorco;user id=freedb_user001;password=pk6rmPza!vD4MGY;charset=utf8";
 
         public int Id { get; set; }
@@ -20,15 +22,40 @@ namespace PetLifeApp.Models
         public string Estado { get; set; }
         public string Telefone { get; set; }
 
+        private void Insercao(string sql)
+        {
+            using (MySqlConnection con = new MySqlConnection(conn))
+            {
+                con.Open();
+                using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                con.Close();
+            }
+        }
+
         public void NovoCliente(Cliente cliente)
         {
             try
             {
-                string sql = "INSERT INTO cliente (nome, dataNascimento) VALUES ("+cliente.Nome+", "+cliente.dataNascimento+")";
-                MySqlConnection con = new MySqlConnection(conn);
-                con.Open();
-                MySqlCommand cmd = new MySqlCommand(sql, con);
-                cmd.ExecuteNonQuery();
+                Insercao("INSERT INTO cliente (nome, dataNascimento) VALUES (" + cliente.Nome + ", " + cliente.dataNascimento + ")");
+
+                using (MySqlConnection con = new MySqlConnection())
+                {
+                    string sql = "SELECT clienteId FROM cliente WHERE nome=" + cliente.Nome + "";
+
+                    con.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                    {
+                        id = (int)cmd.ExecuteScalar();
+                    }
+                    con.Close();
+                }
+
+                Insercao("INSERT INTO endereco (clienteId, rua, numero, cep, cidade, estado) VALUES (" + id + ", " + cliente.Rua + ", " + cliente.Numero + ", " + cliente.Cep + ", " + cliente.Cidade + ", " + cliente.Estado + ", )");
+
+
 
                 //inserir endereço
                 //inserir email
