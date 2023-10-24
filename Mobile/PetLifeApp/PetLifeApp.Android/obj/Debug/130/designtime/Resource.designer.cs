c972db2e608c885d6b29308361751cdabbfcac26
mateushflira <mateushenriquefl@outlook.com>
2023-10-23 21:37:40 -0300

    alterações pagealimentador

--- Mobile/PetLifeApp/PetLifeApp.Android/obj/Debug/130/designtime/Resource.designer.cs
+++ Mobile/PetLifeApp/PetLifeApp.Android/obj/Debug/130/designtime/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace PetLifeApp.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.1.111")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.0.93")]
 	public partial class Resource
 	{
 		
@@ -16658,19 +16658,25 @@ namespace PetLifeApp.Droid
 			public const int pote_agua = 2131165325;
 			
 			// aapt resource value: 0x7F07008E
-			public const int pote_racao = 2131165326;
+			public const int pote_agua1 = 2131165326;
 			
 			// aapt resource value: 0x7F07008F
-			public const int preference_list_divider_material = 2131165327;
+			public const int pote_racao = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int test_custom_background = 2131165328;
+			public const int pote_racao1 = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int tooltip_frame_dark = 2131165329;
+			public const int preference_list_divider_material = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int tooltip_frame_light = 2131165330;
+			public const int test_custom_background = 2131165330;
+			
+			// aapt resource value: 0x7F070093
+			public const int tooltip_frame_dark = 2131165331;
+			
+			// aapt resource value: 0x7F070094
+			public const int tooltip_frame_light = 2131165332;
 			
 			static Drawable()
 			{
--- Mobile/PetLifeApp/PetLifeApp/MainPage.xaml.cs
+++ Mobile/PetLifeApp/PetLifeApp/MainPage.xaml.cs
@@ -10,33 +10,7 @@ namespace PetLifeApp
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void bt_Clicked(object sender, EventArgs e)
-        {
-            Cliente teste = new Cliente()
-            {
-                Nome = "Teste 01",
-                dataNascimento = DateTime.Now,
-                Email = "teste@teste.com",
-                Rua = "Av Brasil",
-                Numero = 564,
-                Cep = "87043698",
-                Cidade = "Maringá",
-                Estado = "PR",
-                Telefone = "(44) 9 9874-2145"
-            };
-
-            try
-            {
-                teste.NovoCliente();
-                DisplayAlert("Sucesso", "Deu bom :)", "OK");
-            }
-            catch (Exception ex)
-            {
-                DisplayAlert("Erro", $"{ex.Message}", "OK");
-            }
-        }
+        }        
 
 		private async void Alimentador_Clicked(object sender, EventArgs e)
 		{
